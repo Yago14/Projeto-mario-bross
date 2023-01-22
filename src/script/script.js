4e95aac6c055b2d162656a0d9df6562fe0440b46
Yago Thyerrir <97457455+Yago14@users.noreply.github.com>
2023-01-21 22:02:58 -0300

    Atualização do projeto

--- src/script/script.js
+++ src/script/script.js
@@ -1,14 +1,19 @@
 const trailer = document.querySelector('.ver-trailer')
 const modal =document.querySelector(".modal")
 const fecharmodal =document.querySelector(".fechar-modal")
+const video = document.querySelector('#video')
+const link = video.src;
 
 
 
 trailer.addEventListener("click",()=>{
      
       modal.classList.add('aberto')
+      video.setAttribute("src",link)
     
 })
 fecharmodal.addEventListener("click",()=>{
-      modal.classList.remove('aberto')
+      modal.classList.remove('aberto');
+      video.setAttribute('src',"")
+      
 })
